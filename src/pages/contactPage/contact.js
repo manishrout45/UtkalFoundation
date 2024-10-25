a11d44935c4741285bd62e3a8236e6fe8dcf0b2c
manishrout45 <manishkumarrout45@gmail.com>
2024-10-26 00:18:37 +0530

    codes updated

--- src/pages/contactPage/contact.js
+++ src/pages/contactPage/contact.js
@@ -17,3 +17,27 @@
             alert("Please fill in all required fields.");
         }
     });
+
+
+
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent default form submission
+    
+        const firstName = document.getElementById('firstName').value;
+        const fatherName = document.getElementById('lastName').value;
+        const motherName = document.getElementById('email').value;
+        const phone = document.getElementById('phone').value;
+        const address = document.getElementById('message').value;
+
+    
+        // Validate if all required fields are filled
+        if (!firstName || !lastName || !email || !phone || !message ) {
+            alert('Please fill out all required fields.');
+            return;
+        }
+    
+        // Display confirmation alert for successful submission
+        alert(`Thank you, ${firstName}! Your message successfully submitted.`);
+    });
+    
+    
